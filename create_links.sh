83b6612286835288355f74a6e5457eda40ed5bfb
Freggar None <Freggar@gmail.com>
2017-05-01 17:20:39 +0200

    Add emacs config to symbolic link script

--- create_links.sh
+++ create_links.sh
@@ -2,3 +2,4 @@
 ln -s $XDG_CONFIG_HOME/vim/vimrc $HOME/.vimrc
 ln -s $XDG_CONFIG_HOME/XOrg/Xdefaults ~/.Xdefaults
 ln -s $XDG_CONFIG_HOME/XOrg/xinitrc ~/.xinitrc
+ln -s $XDG_CONFIG_HOME/emacs/emacs ~/.emacs
