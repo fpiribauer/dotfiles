735f5d1b9dc4de3edba832f09b7e0174f9baebba
Freggar <Freggar@gmail.com>
2018-05-08 20:48:32 +0200

    Add Xmodmap to repo

--- create_links.sh
+++ create_links.sh
@@ -2,6 +2,7 @@
 ln -Ts $XDG_CONFIG_HOME/vim/vimrc $HOME/.vimrc
 ln -Ts $XDG_CONFIG_HOME/XOrg/Xdefaults ~/.Xdefaults
 ln -Ts $XDG_CONFIG_HOME/XOrg/xinitrc ~/.xinitrc
+ln -Ts $XDG_CONFIG_HOME/XOrg/Xmodmap ~/.Xmodmap
 ln -Ts $XDG_CONFIG_HOME/emacs/emacs ~/.emacs
 ln -Ts $XDG_CONFIG_HOME/zsh/zshrc ~/.zshrc
 ln -Ts $XDG_CONFIG_HOME/zsh/zprofile ~/.zprofile
