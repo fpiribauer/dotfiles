ecd80193077f3929a1c9f3ca344f560582753d32
Freggar <Freggar@gmail.com>
2018-07-18 22:29:04 +0200

    Passe Display namen an: Wenn XOrg.conf definiert ist, aendern sich die namen

--- create_links.sh
+++ create_links.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 ln -Ts $XDG_CONFIG_HOME/vim/vimrc $HOME/.vimrc
 ln -Ts $XDG_CONFIG_HOME/XOrg/Xdefaults ~/.Xdefaults
 ln -Ts $XDG_CONFIG_HOME/XOrg/xinitrc ~/.xinitrc
