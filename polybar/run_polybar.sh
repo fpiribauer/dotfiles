920d039d13f4dfa39cf64e6a3c0e97a04938c512
Fabian Piribauer <fabian.piribauer@gmail.com>
2024-02-05 11:56:20 +0100

    remove old configs

--- polybar/run_polybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-killall polybar
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-/home/piri/.config}
-polybar bar1 -c $XDG_CONFIG_HOME/polybar/config &
-polybar bar2 -c $XDG_CONFIG_HOME/polybar/config &
-polybar topbar1 -c $XDG_CONFIG_HOME/polybar/config &
-polybar topbar2 -c $XDG_CONFIG_HOME/polybar/config &
